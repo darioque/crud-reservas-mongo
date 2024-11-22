c8963ee1952a378cde1566fd2118cbc563c25355
Caroamarfil <caroamarfil@outlook.com>
2024-11-21 22:57:46 -0300

    .config corregido

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const PORT = process.env.PORT || 3000;
 export const DB_HOST = process.env.DB_HOST || 'localhost';
 export const DB_PORT = process.env.DB_PORT || 27017;
@@ -5,5 +8,4 @@ export const DB_NAME = process.env.DB_NAME || 'reservasdb';
 export const JWT_SECRET = process.env.JWT_SECRET || 'v8virtuosos';
 
 export const MONGODB_URI =
-  process.env.MONGODB_URI ||
-  `mongodb+srv://Admin:admin789@cluster0.p7mwv.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+  process.env.MONGODB_URI;
